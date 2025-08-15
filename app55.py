29ac5efcc676f12527f966e816fa892d6e7f96bf
Matthew Kim <tmkim1216@gmail.com>
2025-08-15 10:04:20 -0700

    Edit to app55

--- app55.py
+++ app55.py
@@ -1177,8 +1177,7 @@ def generate_memo_from_drive(deal_name):
                     tmp_file.write(file_bytes_io.getvalue())
                     temp_path = tmp_file.name
                 try:
-                    with open(temp_path, "rb") as f:
-                        gemini_inputs.append(genai.upload_file(f, mime_type='application/pdf'))
+                    gemini_inputs.append(genai.upload_file(f, mime_type='application/pdf'))
                 finally:
                     os.unlink(temp_path)
 
